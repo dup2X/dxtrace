65a052d5edbe23f2d66d60edd9c1097aae6456ed
dup2X <dup2snow@gmail.com>
2017-06-07 20:19:00 +0800

    init demo

--- dxtrace.go
+++ dxtrace.go
@@ -23,7 +23,7 @@ func init() {
 						continue
 					}
 					var head = []byte("SCHED")
-					Test(append(head, datas[i]...))
+					report(append(head, datas[i]...))
 				}
 			}
 			os.Truncate(tmpPath, 0)
@@ -33,8 +33,7 @@ func init() {
 
 var tmpPath = "/tmp/xdebug"
 
-// Test just for demo
-func Test(data []byte) {
+func report(data []byte) {
 	parser := &textParser{}
 	r, err := parser.Parse(data)
 	if err != nil {
